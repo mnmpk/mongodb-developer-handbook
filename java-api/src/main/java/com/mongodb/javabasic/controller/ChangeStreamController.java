f3822422658450a8f18cff493df548cf2f3a87f1
mnmpk <mnmpkm@gmail.com>
2024-08-16 18:07:12 +0800

    add change stream

---
+++ java-api/src/main/java/com/mongodb/javabasic/controller/ChangeStreamController.java
@@ -0,0 +1,73 @@
+package com.mongodb.javabasic.controller;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.bson.BsonTimestamp;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.client.ChangeStreamIterable;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.changestream.FullDocument;
+import com.mongodb.javabasic.model.ChangeStreamProcess;
+import com.mongodb.javabasic.model.ChangeStreamProcessConfig;
+import com.mongodb.javabasic.service.ChangeStreamService;
+
+@RestController
+@RequestMapping(path = "/watch")
+public class ChangeStreamController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private ChangeStreamService<Document> changeStreamService;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @RequestMapping("/{collection}")
+    public void watch(@PathVariable("collection") String collection,
+            @RequestParam(required = false, defaultValue = "10") int batchSize,
+            @RequestParam(required = false, defaultValue = "10") int maxAwaitTime,
+            @RequestParam(required = false) boolean resume,
+            @RequestParam(required = false) long startAt,
+            @RequestParam(required = false) long lastEventTime,
+            @RequestParam(required = false, defaultValue = "1") int noOfChangeStream,
+            @RequestParam(required = false, defaultValue = "false") boolean fullDocument) throws Exception {
+        changeStreamService.run(noOfChangeStream, (ChangeStreamProcessConfig<Document> config) -> {
+            List<Bson> pipeline = (List.of(Aggregates.match(
+                    Filters.in("ns.coll", List.of(collection)))));
+            if (startAt > 0)
+                config.setStartAt(new BsonTimestamp(startAt));
+            if (lastEventTime > 0)
+                config.setEndAt(new BsonTimestamp(lastEventTime));
+            return new ChangeStreamProcess<Document>(config,
+                    (e) -> {
+                        logger.info("Body:" + e.getFullDocument());
+                    }) {
+                @Override
+                public ChangeStreamIterable<Document> initChangeStream(List<Bson> p) {
+                    if (pipeline != null && pipeline.size() > 0)
+                        p.addAll(pipeline);
+                    ChangeStreamIterable<Document> changeStream = mongoTemplate.getDb().watch(p, Document.class)
+                            .batchSize(batchSize)
+                            .maxAwaitTime(maxAwaitTime, TimeUnit.MILLISECONDS);
+                    if (fullDocument) {
+                        changeStream = changeStream.fullDocument(FullDocument.UPDATE_LOOKUP);
+                    }
+                    return changeStream;
+                }
+
+            };
+        }, resume);
+    }
+}
---
+++ java-api/src/main/java/com/mongodb/javabasic/service/ChangeStreamService.java
@@ -0,0 +1,72 @@
+package com.mongodb.javabasic.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.mongodb.javabasic.model.ChangeStreamProcess;
+import com.mongodb.javabasic.model.ChangeStreamProcessConfig;
+
+@Service
+public class ChangeStreamService<T> {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private List<ChangeStreamProcess<T>> changeStreams = new ArrayList<>();
+
+    private ChangeStreamProcess<T> earilest = null;
+    private ChangeStreamProcess<T> latest = null;
+
+    @SuppressWarnings("unchecked")
+    public void run(int noOfChangeStream,
+            Function<ChangeStreamProcessConfig<T>, ChangeStreamProcess<T>> initProcess, boolean resume)
+            throws Exception {
+        if (changeStreams.size() > 0) {
+            throw new Exception("Change stream is already running");
+        }
+        for (int i = 0; i < noOfChangeStream; i++) {
+            run((ChangeStreamProcessConfig<T>) ChangeStreamProcessConfig.builder()
+                    .startAt(resume && earilest != null ? earilest.getClusterTime() : null)
+                    .endAt(resume && latest != null ? latest.getClusterTime() : null)
+                    .noOfChangeStream(noOfChangeStream).changeStreamIndex(i).build(), initProcess);
+        }
+    }
+
+    public void run(
+            Function<ChangeStreamProcessConfig<T>, ChangeStreamProcess<T>> initProcess) throws Exception {
+        // TODO: get instance list
+        var noOfInstances = 2;
+        for (int i = 0; i < noOfInstances; i++) {
+            // TODO: Remote call to instance
+        }
+    }
+
+    public void run(ChangeStreamProcessConfig<T> config,
+            Function<ChangeStreamProcessConfig<T>, ChangeStreamProcess<T>> initProcess) {
+        changeStreams.add(initProcess.apply(config));
+        logger.info((config.getChangeStreamIndex() + 1) + "/" + config.getNoOfChangeStream() + ": Run Change stream");
+        new Thread(changeStreams.get(config.getChangeStreamIndex())).start();
+    }
+
+    public void stopAll() throws Exception {
+        for (ChangeStreamProcess<T> process : changeStreams) {
+            process.stop();
+        }
+        // Wait until all threads stopped completed
+        while (!changeStreams.stream().allMatch(cs -> cs.isDone())) {
+            Thread.sleep(100);
+        }
+        earilest = null;
+        latest = null;
+        for (ChangeStreamProcess<T> process : changeStreams) {
+            if (earilest == null || earilest.getClusterTime().compareTo(process.getClusterTime()) > 0)
+                earilest = process;
+            if (latest == null || latest.getClusterTime().compareTo(process.getClusterTime()) < 0)
+                latest = process;
+        }
+        logger.info("earilest:" + earilest.getClusterTime() + " latest:" + latest.getClusterTime());
+        changeStreams.clear();
+    }
+}
