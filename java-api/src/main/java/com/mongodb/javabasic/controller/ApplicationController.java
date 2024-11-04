07f8cdbcafa0073b98257f38adc7540e751ce825
mnmpk <mnmpkm@gmail.com>
2024-11-04 10:59:30 +0800

    test

--- java-api/src/main/java/com/mongodb/javabasic/controller/ApplicationController.java
+++ java-api/src/main/java/com/mongodb/javabasic/controller/ApplicationController.java
@@ -1,12 +1,39 @@
 package com.mongodb.javabasic.controller;
 
+import java.util.LinkedHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.javabasic.model.CustomEntity;
+import com.mongodb.javabasic.repositories.CustomEntityRepository;
+
 @RestController
 public class ApplicationController {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    @Autowired
+    private CustomEntityRepository repository;
     @GetMapping("/test")
     public String test() {
+
+    logger.info(mongoTemplate.getDb().getReadConcern().toString());
+    logger.info(mongoTemplate.getDb().getReadPreference().toString());
+    logger.info(mongoTemplate.getDb().getWriteConcern().toString());
+
+    CustomEntity e = new CustomEntity();
+    e.setId("1234");
+    LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+    map.put("test", "test"+Math.random());
+    e.setData(map);
+    logger.info(e.toString());
+    repository.save(e);
+    logger.info(repository.findById("1234").toString());
         return "OK";
     }
 
