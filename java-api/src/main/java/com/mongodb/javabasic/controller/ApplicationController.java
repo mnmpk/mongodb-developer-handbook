c38a0ec62d1b5801ea2c40201076fda7dba30701
mnmpk <mnmpkm@gmail.com>
2024-11-11 21:52:51 +0800

    for health check

--- java-api/src/main/java/com/mongodb/javabasic/controller/ApplicationController.java
+++ java-api/src/main/java/com/mongodb/javabasic/controller/ApplicationController.java
@@ -7,18 +7,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mongodb.javabasic.model.CustomEntity;
 import com.mongodb.javabasic.repositories.CustomEntityRepository;
 
 @RestController
+@RequestMapping(path = "/")
 public class ApplicationController {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private MongoTemplate mongoTemplate;
     @Autowired
     private CustomEntityRepository repository;
+
+
+	@GetMapping
+	public String health() {
+		return "OK";
+	}
+
     @GetMapping("/test")
     public String test() {
 
