0df1eaea1e8694dd54e7c1b72a2bf22ff522252c
mnmpk <mnmpkm@gmail.com>
2025-03-14 19:25:33 +0800

    1t

--- java-api/src/main/java/com/mongodb/javabasic/controller/PTESController.java
+++ java-api/src/main/java/com/mongodb/javabasic/controller/PTESController.java
@@ -1,9 +1,11 @@
 package com.mongodb.javabasic.controller;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +24,7 @@ import com.mongodb.javabasic.service.AggregationService;
 import com.mongodb.javabasic.model.Stop;
 import com.mongodb.javabasic.model.Suggestion;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.geojson.MultiPoint;
 import com.mongodb.client.model.geojson.Point;
 import com.mongodb.client.model.geojson.Position;
 import com.mongodb.javabasic.model.Route;
@@ -45,51 +48,84 @@ public class PTESController {
     @GetMapping("/stops")
     public List<Stop> getStops(@RequestParam("lat") double lat, @RequestParam("lng") double lng) {
         return mongoTemplate.getCollection(mongoTemplate.getCollectionName(Stop.class)).withDocumentClass(Stop.class)
-                .find(Filters.nearSphere("location", new Point(new Position(lng, lat)), 100d, 0d)).into(new ArrayList<>());
+                .find(Filters.nearSphere("location", new Point(new Position(lng, lat)), 100d, 0d))
+                .into(new ArrayList<>());
         // return stopRepository.findAll();
     }
 
-    /*@GetMapping("/routes")
-    public List<Route> getRoutes() {
-        return routeRepository.findAll();
-    }*/
+    /*
+     * @GetMapping("/routes")
+     * public List<Route> getRoutes() {
+     * return routeRepository.findAll();
+     * }
+     */
 
     @GetMapping("/routes")
     public List<Route> getRoutes(@RequestParam("lat") double lat, @RequestParam("lng") double lng) {
-        return aggregationService.getPipelineResults(mongoTemplate.getCollectionName(Route.class), "direct-route.json", Route.class, Map.of("lat",lat,"lng",lng));
-        //return mongoTemplate.getCollection(mongoTemplate.getCollectionName(Route.class)).withDocumentClass(Route.class)
-        //        .find(Filters.nearSphere("stops.location", new Point(new Position(lng, lat)), 200d, 0d)).into(new ArrayList<>());
+        return aggregationService.getPipelineResults(mongoTemplate.getCollectionName(Route.class), "direct-route.json",
+                Route.class, Map.of("lat", lat, "lng", lng));
+        // return
+        // mongoTemplate.getCollection(mongoTemplate.getCollectionName(Route.class)).withDocumentClass(Route.class)
+        // .find(Filters.nearSphere("stops.location", new Point(new Position(lng, lat)),
+        // 200d, 0d)).into(new ArrayList<>());
     }
 
     @PostMapping("/routes")
     public List<Suggestion> getRoutes(double[] start, double[] end) {
+        List<Route> startRoutes = this.getRoutes(start[1], start[0]);
+        List<Route> endRoutes = this.getRoutes(end[1], end[0]);
         List<Suggestion> suggestions = new ArrayList<>();
-        suggestions.addAll(this.getDirectRoutes(start, end));
-        suggestions.addAll(this.get1TRoutes());
+
+        //suggestions.addAll(this.getDirectRoutes(startRoutes, endRoutes));
+
+        suggestions.addAll(this.get1TRoutes(startRoutes, endRoutes));
         suggestions.addAll(this.get2TRoutes());
         return suggestions;
     }
 
-    private List<Suggestion> getDirectRoutes(double[] start, double[] end) {
-        List<Route> startRoutes = this.getRoutes(start[1], start[0]);
-        List<Route> endRoutes = this.getRoutes(end[1], end[0]);
+    private List<Suggestion> getDirectRoutes(List<Route> startRoutes, List<Route> endRoutes) {
         List<Route> list = new ArrayList<>();
-        startRoutes.forEach(r->{
-            endRoutes.forEach(r2->{
-                if(r.getRoute().equalsIgnoreCase(r2.getRoute()) && 
-                r.getBound().equalsIgnoreCase(r2.getBound()) &&
-                r.getServiceType().equalsIgnoreCase(r2.getServiceType()) &&
-                r.getStopIndex()<r2.getStopIndex()){
-                        list.add(r);
+        startRoutes.forEach(r -> {
+            endRoutes.forEach(r2 -> {
+                if (r.getRoute().equalsIgnoreCase(r2.getRoute()) &&
+                        r.getBound().equalsIgnoreCase(r2.getBound()) &&
+                        r.getServiceType().equalsIgnoreCase(r2.getServiceType()) &&
+                        r.getStopIndex() < r2.getStopIndex()) {
+                    list.add(r);
                 }
             });
         });
         return list.stream().map(r -> Suggestion.builder().legs(List.of(r)).build()).toList();
     }
 
-    private List<Suggestion> get1TRoutes() {
-        mongoTemplate.getCollection(mongoTemplate.getCollectionName(Route.class));
-        return List.of();
+    private List<Suggestion> get1TRoutes(List<Route> startRoutes, List<Route> endRoutes) {
+        Set<Position> transferStops = new HashSet<>();
+        startRoutes.stream().forEach(r -> {
+            r.getStops().forEach(s -> {
+                if (!s.getLocation().equals(r.getNearestStop())) {
+                    transferStops.add(s.getLocation().getPosition());
+                }
+            });
+        });
+        List<Route> intermediateRoute = mongoTemplate.getCollection(mongoTemplate.getCollectionName(Route.class)).withDocumentClass(Route.class)
+                .find(Filters.geoIntersects("stops.location",
+                        new MultiPoint(transferStops.stream().collect(Collectors.toList()))))
+                .into(new ArrayList<>());
+
+        List<Route> list = new ArrayList<>();
+        intermediateRoute.forEach(r -> {
+            endRoutes.forEach(r2 -> {
+                if (r.getRoute().equalsIgnoreCase(r2.getRoute()) &&
+                        r.getBound().equalsIgnoreCase(r2.getBound()) &&
+                        r.getServiceType().equalsIgnoreCase(r2.getServiceType())
+                        // && r.getStopIndex() < r2.getStopIndex() // Need to find alternative way
+                         ) {
+                    list.add(r);
+                }
+            });
+        });
+logger.info(list.toString());
+        return list.stream().map(r -> Suggestion.builder().legs(List.of(r)).build()).toList();
     }
 
     private List<Suggestion> get2TRoutes() {
