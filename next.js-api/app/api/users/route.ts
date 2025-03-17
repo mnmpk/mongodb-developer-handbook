806c9b1f46e1813de9652c8b8362d01d1109e6b3
mnmpk <mnmpkm@gmail.com>
2025-03-17 11:06:22 +0800

    mongodb test

--- next.js-api/app/api/users/route.ts
+++ next.js-api/app/api/users/route.ts
@@ -1,8 +1,11 @@
 import { putSession, readSession } from "@/app/lib/session";
-
+import { MongoClient } from "mongodb";
+const uri = "mongodb+srv://admin:admin12345@demo.uskpz.mongodb.net/mongodb-developer";
+const client = new MongoClient(uri);
 
 export async function GET(request: Request) {
     let session: any = readSession();
+    console.log(await client.db().collection("sessions").find());
     console.log(await session);
     putSession({ userId: '123' });
     // For example, fetch data from your DB here
