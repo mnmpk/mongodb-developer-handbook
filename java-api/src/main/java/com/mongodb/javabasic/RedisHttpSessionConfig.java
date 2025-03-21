3f648df58ac3f73b9312c0dfc998288471a336d2
m-mpk <m_ma@mzinx.com>
2025-03-22 03:31:38 +0800

    add redis session

---
+++ java-api/src/main/java/com/mongodb/javabasic/RedisHttpSessionConfig.java
@@ -0,0 +1,12 @@
+package com.mongodb.javabasic;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+//@Configuration(proxyBeanMethods = false)
+//@EnableRedisHttpSession
+public class RedisHttpSessionConfig {
+
+
+}
