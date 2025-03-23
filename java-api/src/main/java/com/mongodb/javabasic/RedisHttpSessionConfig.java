02f800ecba678f3d7d7eb312141c4bf1f771fc22
mnmpk <mnmpkm@gmail.com>
2025-03-23 15:21:38 +0800

    test

--- java-api/src/main/java/com/mongodb/javabasic/RedisHttpSessionConfig.java
+++ java-api/src/main/java/com/mongodb/javabasic/RedisHttpSessionConfig.java
@@ -1,15 +1,18 @@
 package com.mongodb.javabasic;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
-
 
 @Profile("redis")
 @Configuration(proxyBeanMethods = false)
 @EnableRedisIndexedHttpSession
 public class RedisHttpSessionConfig {
 
-
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 }
