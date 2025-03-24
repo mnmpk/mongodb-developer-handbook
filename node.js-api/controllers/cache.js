1ca73e7bf26ddf6cd8d15104681823a229665691
mnmpk <mnmpkm@gmail.com>
2025-03-24 11:33:36 +0800

    string

--- node.js-api/controllers/cache.js
+++ node.js-api/controllers/cache.js
@@ -5,8 +5,18 @@ const init = (conn) => {
 }
 const put = async (req, res) => {
     const coll = db.collection('cacheObject');
-    const o = { value: require('node:crypto').randomBytes(1 * 1024 * 1024 / 3).toString() };
+    const o = { value: generateRandomString(req.query['size']).toString() };
     await coll.insertOne(o)
     res.send(o);
 }
 module.exports = { init, put };
+
+function generateRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
